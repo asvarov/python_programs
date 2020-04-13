f82b2bb0f54b8d37a1a7895ed30a8fc7f3bda0d3
asvarov <45462669+asvarov@users.noreply.github.com>
2020-04-13 21:13:44 +0300

    Update substitutor.py

--- substitutor.py
+++ substitutor.py
@@ -1,4 +1,5 @@
-import re, os
+import re
+import os
 
 
 def re_subs(from_string, to_string, source, destination='results', file_zones='zones'):
@@ -17,14 +18,14 @@ def re_subs(from_string, to_string, source, destination='results', file_zones='z
 
     try:
         os.mkdir(destination)
-        print("Directory created. New files saved in folder '" + destination + "'", sep='')
+        print("Directory created. New files have been saved in folder '" + destination + "'", sep='')
     except FileExistsError:
         pass
 
     for i in list_:
         try:
             data = open(source + '/' + i).read()
-            o = open(destination + '/' + i, 'w')
+            o = open(destination + '/' + i, 'w', encoding="utf-8")
             o.write(re.sub(from_string, to_string, data))
             o.close()
         except FileNotFoundError as error:
@@ -33,10 +34,10 @@ def re_subs(from_string, to_string, source, destination='results', file_zones='z
 
 def main():
     source = 'data.db'
-    re_subs('IN SOA	ispm.ukrtel.net. root.ispm.ukrtel.net.', 'IN SOA	dispm.ukrtel.net. root.dispm.ukrtel.net.', source)
-    re_subs('IN SOA	ispm.ukrtel.net. support-dc.ukrtelecom.ua.', 'IN SOA	dispm.ukrtel.net. root.dispm.ukrtel.net.', source)
-    re_subs('NS	isp1.ukrtel.net.', '; NS	isp1.ukrtel.net.', source)
-    re_subs('NS	isp2.ukrtel.net.', 'NS	dispm.ukrtel.net.', source)
+    re_subs('IN SOA\tispm.ukrtel.net. root.ispm.ukrtel.net.', 'IN SOA\tdispm.ukrtel.net. root.dispm.ukrtel.net.', source)
+    re_subs('IN SOA\tispm.ukrtel.net. support-dc.ukrtelecom.ua.', 'IN SOA\tdispm.ukrtel.net. root.dispm.ukrtel.net.', source)
+    re_subs('NS	isp1.ukrtel.net.', '; NS\tisp1.ukrtel.net.', source)
+    re_subs('NS	isp2.ukrtel.net.', 'NS\tdispm.ukrtel.net.', source)
 
 
 if __name__ == '__main__':
