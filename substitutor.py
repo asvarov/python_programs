75751a6f2c1f472bd86f7bc344c00a07f127d8bf
asvarov <45462669+asvarov@users.noreply.github.com>
2020-04-14 21:45:31 +0300

    Update substitutor.py

--- substitutor.py
+++ substitutor.py
@@ -2,12 +2,11 @@ import re
 import os
 
 
-def re_subs(from_string, to_string, source, destination='results', file_zones='zones'):
+def re_subs(from_string, to_string, source, file_zones='zones'):
     """ Функция замены текста в файлах из списка "l":
-        source  - папка где расположены файлы в которых нужно заменять текст
-        destination  - папка куда сохранять файлы в которых текст изменен (по умолчанию 'results')
         from_string  - текст который нужно заменять
         to_string  - текст на который нужно заменять
+        source  - папка где расположены файлы в которых нужно заменять текст
         file_zones - имя файла в котором хранится список файлов которые нужно обработать (по умолчанию 'zones')"""
     list_ = []
     try:
@@ -16,28 +15,23 @@ def re_subs(from_string, to_string, source, destination='results', file_zones='z
     except FileNotFoundError as error:
         print('Файл со списком не найден:', error)
 
-    try:
-        os.mkdir(destination)
-        print("Directory created. New files have been saved in folder '" + destination + "'", sep='')
-    except FileExistsError:
-        pass
-
     for i in list_:
         try:
-            data = open(source + '/' + i).read()
-            o = open(destination + '/' + i, 'w', encoding="utf-8")
-            o.write(re.sub(from_string, to_string, data))
-            o.close()
+            path_read_file = os.path.join(source, i)
+            read_data = open(path_read_file).read()
+            path_write_file = open(path_read_file, 'w', encoding='utf-8')
+            path_write_file.write(re.sub(from_string, to_string, read_data))
+            path_write_file.close()
         except FileNotFoundError as error:
-            print('Error:', error)
+            print('Error: No such file or directory:', os.path.join(source, i))
 
 
 def main():
     source = 'data.db'
     re_subs('IN SOA\tispm.ukrtel.net. root.ispm.ukrtel.net.', 'IN SOA\tdispm.ukrtel.net. root.dispm.ukrtel.net.', source)
     re_subs('IN SOA\tispm.ukrtel.net. support-dc.ukrtelecom.ua.', 'IN SOA\tdispm.ukrtel.net. root.dispm.ukrtel.net.', source)
-    re_subs('NS	isp1.ukrtel.net.', '; NS\tisp1.ukrtel.net.', source)
-    re_subs('NS	isp2.ukrtel.net.', 'NS\tdispm.ukrtel.net.', source)
+    re_subs('\tNS\tisp1.ukrtel.net.', '\t;NS\tisp1.ukrtel.net.', source)
+    re_subs('\tNS\tisp2.ukrtel.net.', '\tNS\tdispm.ukrtel.net.', source)
 
 
 if __name__ == '__main__':
